42745d148fdc0c7e5c87c11f1edbc50b1ae22b01
Cesar1505 <cesar.zela6321@utc.edu.ec>
2025-07-09 16:30:49 -0500

    configuracion de las rutas para punto

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PuntoController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+// habilitar una ruta especifica para el mapa 
+Route::get('/puntos/mapa',[PuntoController::class,'mapa']);
+
+
+//habilitando acceso al controlador 
+Route::resource('puntos',PuntoController::class);
+
