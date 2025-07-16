4a79a694abc13f20b510538e767439c1ebefcdf5
Cesar1505 <cesar.zela6321@utc.edu.ec>
2025-07-16 04:10:05 -0500

    model de zonas de riesgo

--- app/Models/Riesgo.php
+++ app/Models/Riesgo.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Riesgo extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'nivel_riesgo',
+        'latitud1', 'longitud1',
+        'latitud2', 'longitud2',
+        'latitud3', 'longitud3',
+        'latitud4', 'longitud4',
+    ];
+
 }
--- database/migrations/2025_07_16_090354_create_riesgos_table.php
+++ database/migrations/2025_07_16_090354_create_riesgos_table.php
@@ -12,7 +12,21 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('riesgos', function (Blueprint $table) {
-            $table->id();
+             $table->id();
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->enum('nivel_riesgo', ['bajo', 'medio', 'alto']);
+
+            // Coordenadas del polígono (4 vértices)
+            $table->decimal('latitud1', 10, 7);
+            $table->decimal('longitud1', 10, 7);
+            $table->decimal('latitud2', 10, 7);
+            $table->decimal('longitud2', 10, 7);
+            $table->decimal('latitud3', 10, 7);
+            $table->decimal('longitud3', 10, 7);
+            $table->decimal('latitud4', 10, 7);
+            $table->decimal('longitud4', 10, 7);
+
             $table->timestamps();
         });
     }
