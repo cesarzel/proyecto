a9bff6319cfbb204b80fa7ee7b131328a69e9207
Cesar1505 <cesar.zela6321@utc.edu.ec>
2025-07-16 04:14:18 -0500

    configuracion controller

--- app/Http/Controllers/RiesgoController.php
+++ app/Http/Controllers/RiesgoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Riesgo;
 
 class RiesgoController extends Controller
 {
@@ -11,15 +12,22 @@ class RiesgoController extends Controller
      */
     public function index()
     {
-        //
+        $riesgos = Riesgo::all();
+        return view('riesgos.index', compact('riesgos'));
     }
 
+
+    public function mapa()
+    {
+        $riesgos = Riesgo::all();
+        return view('riesgos.mapa', compact('riesgos'));
+    }
     /**
      * Show the form for creating a new resource.
      */
     public function create()
     {
-        //
+        return view('riesgos.nuevo');
     }
 
     /**
@@ -27,7 +35,24 @@ class RiesgoController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $datos = [
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'nivel_riesgo' => $request->nivel_riesgo,
+
+            'latitud1' => $request->latitud1,
+            'longitud1' => $request->longitud1,
+            'latitud2' => $request->latitud2,
+            'longitud2' => $request->longitud2,
+            'latitud3' => $request->latitud3,
+            'longitud3' => $request->longitud3,
+            'latitud4' => $request->latitud4,
+            'longitud4' => $request->longitud4,
+        ];
+
+        Riesgo::create($datos);
+
+        return redirect()->route('riesgos.index')->with('success', 'Zona de riesgo registrada correctamente');
     }
 
     /**
@@ -43,7 +68,8 @@ class RiesgoController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $riesgo = Riesgo::findOrFail($id);
+        return view('riesgos.editar', compact('riesgo'));
     }
 
     /**
@@ -51,7 +77,24 @@ class RiesgoController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+         $riesgo = Riesgo::findOrFail($id);
+
+        $riesgo->update([
+            'nombre' => $request->nombre,
+            'descripcion' => $request->descripcion,
+            'nivel_riesgo' => $request->nivel_riesgo,
+
+            'latitud1' => $request->latitud1,
+            'longitud1' => $request->longitud1,
+            'latitud2' => $request->latitud2,
+            'longitud2' => $request->longitud2,
+            'latitud3' => $request->latitud3,
+            'longitud3' => $request->longitud3,
+            'latitud4' => $request->latitud4,
+            'longitud4' => $request->longitud4,
+        ]);
+
+        return redirect()->route('riesgos.index')->with('success', 'Zona de riesgo actualizada correctamente');
     }
 
     /**
@@ -59,6 +102,9 @@ class RiesgoController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $riesgo = Riesgo::findOrFail($id);
+        $riesgo->delete();
+
+        return redirect()->route('riesgos.index')->with('success', 'Zona de riesgo eliminada correctamente');
     }
 }
