3d503d107a06a97eafcbca1c80dc856a9b52a422
Cesar1505 <cesar.zela6321@utc.edu.ec>
2025-07-16 21:43:37 -0500

    creacion del rol visitante

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PuntoController;
-use App\Http\Controllers\RiesgoController;
-use App\Http\Controllers\ReportePDFController;
-use App\Http\Controllers\SeguraController;
-use App\Models\Punto;
-use App\Models\Riesgo;
-use App\Models\Segura;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/mapa-general', function () {
-    $puntos = Punto::all();
-    $riesgos = Riesgo::all();
-    $seguras = Segura::all();
-    return view('mapa_general', compact('puntos', 'riesgos', 'seguras'));
-})->name('mapa.general');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-
-
-// habilitar una ruta especifica para el mapa 
-Route::get('/puntos/mapa',[PuntoController::class,'mapa']);
-Route::get('/riesgos/mapa', [RiesgoController::class, 'mapa'])->name('riesgos.mapa');
-Route::get('seguras/mapa', [SeguraController::class, 'mapa'])->name('seguras.mapa');
-
-
-
-//habilitando acceso al controlador 
-Route::resource('puntos',PuntoController::class);
-Route::resource('riesgos', RiesgoController::class);
-Route::resource('seguras', SeguraController::class);
-
-Route::get('/punto/template', function () {
-    return view('puntos.mytemplate');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/reporte/pdf', [ReportePDFController::class, 'generarReporte'])->name('reporte.pdf');
+require __DIR__.'/auth.php';
