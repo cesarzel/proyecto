326ba348454ee1378230cfa1aedc235fedce5368
isac666 <isaaccisnerosd@gmail.com>
2025-07-16 23:51:50 -0500

    mapa points

--- resources/views/puntos/mytemplate.blade.php
+++ resources/views/puntos/mytemplate.blade.php
@@ -105,7 +105,7 @@
 
 
     <!-- About Start -->
-    <div class="container-fluido bg-light overflow-hidden my-5 px-lg-0">
+    <div class="container-fluid bg-light overflow-hidden my-5 px-lg-0">
         <div class="container about px-lg-0">
             <div class="row g-0 mx-lg-0">
                 <div class="col-lg-6 ps-lg-0" style="min-height: 400px;">
