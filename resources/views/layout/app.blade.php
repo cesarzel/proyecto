4ca7f170f33062c9dea46fdc2f6d6c7f5c307ec3
Mike Lopez <michaeldamianlopezsegura@gmail.com>
2025-07-17 00:18:09 -0500

    Centrado de Icono

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,7 +13,7 @@
 
    
 
-    <!-- Google Web Fonts -->
+    <!-- Configuracion de la api -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;500&family=Roboto:wght@500;700;900&display=swap" rel="stylesheet"> 
